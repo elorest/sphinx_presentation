97c33abaaf966e61eb8770e003aad95bbaaa8905
Isaac Sloan <isaac@isaacsloan.com>
2012-09-05 15:36:36 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SphinxPresentation::Application.config.secret_token = '0dff55d638dad0df076d3c482777e8ea71df16c7bf4cd7fa6ad7b61653294919b102b0557ac6ecae96eca220c33aa58076fb25a30fbb46cae898a14eb258b979'

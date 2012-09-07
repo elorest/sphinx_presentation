d8f14c70429067cb5fa4876eb5f753a31df0b1b4
Isaac Sloan <isaac@isaacsloan.com>
2012-09-07 01:30:51 -0600

    indexes

--- db/migrate/20120905215759_create_zipcodes.rb
+++ db/migrate/20120905215759_create_zipcodes.rb
@@ -1,7 +1,7 @@
 class CreateZipcodes < ActiveRecord::Migration
   def change
     create_table :zipcodes do |t|
-      t.integer :zip
+      t.string :zip
       t.string :city
       t.string :state
       t.string :full_state
